cb26d4b2dad3791a00fa817a2df9ab394f69992f
steven.vincentGNR@gmail.com <steven.vincentGNR@gmail.com>
2021-10-21 07:54:33 +0000

    fix chrome driver error

--- 0.Global/instanciate_cluster.py
+++ 0.Global/instanciate_cluster.py
@@ -8,12 +8,27 @@
 # MAGIC sudo apt autoremove -y
 # MAGIC sudo apt-get install chromium-browser -y
 # MAGIC sudo apt-get install chromium-chromedriver -y
-# MAGIC #pip install pandas
-# MAGIC #pip install lxml
-# MAGIC #pip install -q selenium
-# MAGIC #pip install Pillow
-# MAGIC #pip install html5
-# MAGIC #pip install -q beautifulsoup4 
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# MAGIC sudo dpkg -i google-chrome-stable_current_amd64.deb
+# MAGIC sudo apt-get install -f -y
+# MAGIC sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC CHROME_VERSION=$(google-chrome --version | cut -f 3 -d ' ' | cut -d '.' -f 1) \
+# MAGIC         && CHROMEDRIVER_RELEASE=$(curl --location --fail --retry 3 http://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION}) \
+# MAGIC         && curl --silent --show-error --location --fail --retry 3 --output /tmp/chromedriver_linux64.zip "http://chromedriver.storage.googleapis.com/$CHROMEDRIVER_RELEASE/chromedriver_linux64.zip" \
+# MAGIC         && cd /tmp \
+# MAGIC         && unzip chromedriver_linux64.zip \
+# MAGIC         && rm -rf chromedriver_linux64.zip \
+# MAGIC         && sudo mv chromedriver /usr/local/bin/chromedriver \
+# MAGIC         && sudo chmod +x /usr/local/bin/chromedriver \
+# MAGIC         && chromedriver --version
 
 # COMMAND ----------
 
