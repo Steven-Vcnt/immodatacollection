e88f2bcb5ff34db69906fa3da2a4de1880a68cb4
Steven-Vcnt <Steven-Vcnt>
2023-11-18 17:41:44 +0000

    ignore git .deb files and add nb configuration file and delete previous config file

--- app/config/init_worskpace.py
+++ app/config/init_worskpace.py
@@ -1,15 +1,34 @@
-# COMMAND ----------
+# Databricks notebook source
 import sys
-sys.path.append("../app")
+sys.path.append("/Workspace/Repos/steven.vincent@bsb-education.com/immodatacollection/app")
 import util.scrappers as us
 import pandas as pd
 
 # COMMAND ----------
+
+# MAGIC %sh
+# MAGIC sudo apt-get update
+# MAGIC wget http://ftp.de.debian.org/debian/pool/main/f/fonts-liberation/fonts-liberation_1.07.4-11_all.deb
+# MAGIC sudo dpkg -i fonts-liberation_1.07.4-11_all.deb
+# MAGIC wget http://ftp.de.debian.org/debian/pool/main/m/mesa/libgbm1_20.3.5-1_amd64.deb
+# MAGIC sudo dpkg -i libgbm1_20.3.5-1_amd64.deb
+# MAGIC wget http://ftp.de.debian.org/debian/pool/main/n/nspr/libnspr4_4.29-1_amd64.deb
+# MAGIC sudo dpkg -i libnspr4_4.29-1_amd64.deb
+# MAGIC wget http://ftp.de.debian.org/debian/pool/main/n/nss/libnss3_3.61-1+deb11u1_amd64.deb
+# MAGIC sudo dpkg -i libnss3_3.61-1+deb11u1_amd64.deb
+# MAGIC sudo apt-get -f install -y
+# MAGIC wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# MAGIC sudo dpkg -i google-chrome-stable_current_amd64.deb
+# MAGIC sudo apt-get install -f -y
+
+# COMMAND ----------
+
 spark.sql('CREATE DATABASE bronze')
 spark.sql('CREATE DATABASE silver')
 spark.sql('CREATE DATABASE gold')
 
 # COMMAND ----------
+
 # Main Castorus
 url = [
     "https://www.castorus.com/s/Levallois-perret,38504,-------------------------",
@@ -40,34 +59,48 @@ for each in url:
     # Convert Pandas DF to SPark DF, get Castorus Main table and rename column
     sp_MainCastorus = pd.DataFrame(us.GetCastorus.CastorusMainTable(each, headers).rename(columns={"vue le": "vue"}, inplace=False))
     full_main = pd.concat([full_main, sp_MainCastorus])
-    break
 full_main = full_main.drop_duplicates("id")
+full_main['rendmt_brut'] = full_main['rendmt brut']
+del full_main['rendmt brut']
 del full_main['Unnamed: 0']
 # Create SQL view
 full_main["depuis"] = full_main["depuis"].astype(str)
 full_main["piec."] = full_main["piec."].astype(str)
 full_main["m2"] = full_main["m2"].astype(str)
-# COMMAND ----------
-spark.createDataFrame(
+sp_MainCastorus=spark.createDataFrame(
     full_main[full_main["SourceLink"].notnull()].reset_index(drop=True)
-).distinct().createOrReplaceTempView("main_castorus_updates")
-
+)
 sp_MainCastorus.distinct().write.mode("Overwrite").option("OverwriteSchema", "true").format("delta").save("/FileStore/bronze/main_castorus") 
 spark.sql("CREATE TABLE IF NOT EXISTS bronze.main_castorus USING DELTA LOCATION '/FileStore/bronze/main_castorus'")
 
+
 # COMMAND ----------
+
 
 # Castorus Change table
 
-castorus = spark.sql('''SELECT Link, mt.id
-FROM silver.main_table mt
+castorus = spark.sql('''SELECT mc.Link, mc.id
+FROM bronze.main_castorus mc
 ''').limit(1000).toPandas()
 if len(castorus) == 0:
     dbutils.notebook.exit('Success')
 else:
     pd_castorus = us.GetCastorus.getChange(castorus)
     sp_Castorus = spark.createDataFrame(pd_castorus)
-    sp_Castorus.distinct().createOrReplaceTempView('castorus_change_updates')
 
 sp_Castorus.distinct().write.mode("overwrite").option("overwriteschema", "true").format("delta").save("/FileStore/bronze/castorus_change") 
 spark.sql("CREATE TABLE IF NOT EXISTS bronze.castorus_change USING DELTA LOCATION '/FileStore/bronze/castorus_change'")
+
+# COMMAND ----------
+
+pd_castorus
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC SELECT * FROM bronze.castorus_change
+
+# COMMAND ----------
+
+
